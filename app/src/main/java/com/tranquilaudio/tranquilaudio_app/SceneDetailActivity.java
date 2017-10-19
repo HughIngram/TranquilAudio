e3a2b8a3bdab76d9d90fee4b27b3b0ad47c1e2cd
HughIngram <HughIngram@users.noreply.github.com>
2017-10-19 22:14:55 +0100

    test

--- app/src/main/java/com/tranquilaudio/tranquilaudio_app/SceneDetailActivity.java
+++ app/src/main/java/com/tranquilaudio/tranquilaudio_app/SceneDetailActivity.java
@@ -79,15 +79,6 @@ public final class SceneDetailActivity extends AppCompatActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
 
-        // savedInstanceState is non-null when there is fragment state
-        // saved from previous configurations of this activity
-        // (e.g. when rotating the screen from portrait to landscape).
-        // In this case, the fragment will automatically be re-added
-        // to its container so we don't need to manually add it.
-        // For more information, see the Fragments API guide at:
-        //
-        // http://developer.android.com/guide/components/fragments.html
-        //
         if (savedInstanceState == null) {
             // Create the detail fragment and add it to the activity
             // using a fragment transaction.
